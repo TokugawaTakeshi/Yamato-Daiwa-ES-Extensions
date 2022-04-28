f7320c5cfbcb441df74e3e747b6152f946dc8289
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2022-04-28 11:30:09 +0900

    Update 'replaceArrayElementsByIndexesImmutably'

--- CoreLibrary/Package/Source/Arrays/replaceArrayElementsByIndexesImmutably.ts
+++ CoreLibrary/Package/Source/Arrays/replaceArrayElementsByIndexesImmutably.ts
@@ -1,16 +1,28 @@
 export default function replaceArrayElementsByIndexesImmutably<ArrayElement>(
-  namedParameters: {
-    targetArray: Array<ArrayElement>;
-    replacements: Array<{
-      index: number;
-      newElement: ArrayElement;
-    }>;
-  }
+  namedParameters:
+      {
+        targetArray: Array<ArrayElement>;
+      } &
+      (
+        {
+          index: number;
+          newElement: ArrayElement;
+        } |
+        {
+          replacements: Array<{
+            index: number;
+            newElement: ArrayElement;
+          }>;
+        }
+      )
 ): Array<ArrayElement> {
 
   const workpiece: Array<ArrayElement> = [ ...namedParameters.targetArray ];
+  const replacements: Array<{ index: number; newElement: ArrayElement; }> =
+      "replacements" in namedParameters ? namedParameters.replacements :
+      [ { index: namedParameters.index, newElement: namedParameters.newElement } ];
 
-  for (const replacement of namedParameters.replacements) {
+  for (const replacement of replacements) {
     workpiece[replacement.index] = replacement.newElement;
   }
 
