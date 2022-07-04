3a2941560ad50e078efe79f87f7628722eaa1ffe
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2022-07-04 16:27:55 +0900

    Remove became redundant disablings of "@typescript-eslint/ban-types" rule

--- CoreLibrary/Package/Source/TypeGuards/Objects/isEmptyObject.ts
+++ CoreLibrary/Package/Source/TypeGuards/Objects/isEmptyObject.ts
@@ -1,6 +1,3 @@
-/*  〔 ESLint muting rationale 〕 Although "object" is not recommended to use, this check could be interim in narrowing from
-*     'unknown' to object with fixed keys and values types. */
-/* eslint-disable @typescript-eslint/ban-types */
 export default function isEmptyObject(potentialObject: unknown): potentialObject is object {
 
   if (typeof potentialObject !== "object" || potentialObject === null) {
--- CoreLibrary/Package/Source/TypeGuards/Objects/isNonNullObject.ts
+++ CoreLibrary/Package/Source/TypeGuards/Objects/isNonNullObject.ts
@@ -1,6 +1,3 @@
-/*  〔 ESLint muting rationale 〕 Although object is not recommended to use, this check could be interim in narrowing from
-*     'unknown' to object with fixed keys and values types. */
-/* eslint-disable @typescript-eslint/ban-types */
 export default function isNonNullObject(potentialObject: unknown): potentialObject is object {
   return typeof potentialObject === "object" && potentialObject !== null;
 }
