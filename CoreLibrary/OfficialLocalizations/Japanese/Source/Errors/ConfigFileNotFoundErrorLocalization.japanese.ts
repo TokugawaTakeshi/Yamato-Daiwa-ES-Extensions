c6c2be69426815a38ed14a1e471c5e26adc503f0
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2022-09-24 10:57:53 +0900

    * Update localizations of `ConfigFileNotFoundError` * Update localizations of `InvalidParameterValueError`

--- CoreLibrary/OfficialLocalizations/Japanese/Source/Errors/ConfigFileNotFoundErrorLocalization.japanese.ts
+++ CoreLibrary/OfficialLocalizations/Japanese/Source/Errors/ConfigFileNotFoundErrorLocalization.japanese.ts
@@ -1,17 +1,26 @@
 import type { ConfigFileNotFoundError } from "@yamato-daiwa/es-extensions";
+import { insertSubstring } from "@yamato-daiwa/es-extensions";
 
 
 const ConfigFileNotFoundErrorLocalization__Japanese: ConfigFileNotFoundError.Localization = {
   defaultTitle: "設定ファイル未発見",
   generateDescription(namedParameters: ConfigFileNotFoundError.Localization.DescriptionTemplateNamedParameters): string {
 
+    let messageCommonPart: string;
+
     if (Array.isArray(namedParameters.configFilePathOrMultipleOfThem)) {
-      return `「${ namedParameters.targetTechnologyName }」、\n` +
+      messageCommonPart = `「${ namedParameters.targetTechnologyName }」、\n` +
           `${ namedParameters.configFilePathOrMultipleOfThem.join(", \n") }\nの、どちらの設定ファイルも発見出来なかった。`;
+    } else {
+      messageCommonPart = `「${ namedParameters.targetTechnologyName }」の設定ファイル` +
+          `「${ namedParameters.configFilePathOrMultipleOfThem }」が発見出来なかった。`;
     }
 
 
-    return `「${ namedParameters.targetTechnologyName }」の設定ファイル「${ namedParameters.configFilePathOrMultipleOfThem }」が発見出来なかった。`;
+    return `${ messageCommonPart }` +
+        `${ insertSubstring(namedParameters.messageSpecificPart, {
+          modifier: (messageSpecificPart: string): string => `\n${ messageSpecificPart }`
+        }) }`;
   }
 };
 
--- CoreLibrary/OfficialLocalizations/Japanese/Source/Errors/InvalidParameterValueErrorLocalization.japanese.ts
+++ CoreLibrary/OfficialLocalizations/Japanese/Source/Errors/InvalidParameterValueErrorLocalization.japanese.ts
@@ -6,7 +6,7 @@ const InvalidParameterValueErrorLocalization__Japanese: InvalidParameterValueErr
   defaultTitle: "引数の値の不正",
   generateDescription:
       (namedParameters: InvalidParameterValueError.Localization.DescriptionTemplateNamedParameters): string =>
-          `引数「${ namedParameters.parameterName }」が不正な値になっている。` +
+          `${ namedParameters.parameterNumber }引数目（「${ namedParameters.parameterName }」）が不正な値になっている。` +
           `${ insertSubstring(namedParameters.messageSpecificPart, {
             modifier: (messageSpecificPart: string): string => `\n${ messageSpecificPart }`
           }) }`
